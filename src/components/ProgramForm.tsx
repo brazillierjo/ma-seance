b287edc6f6170af72a8005d9bfcae38e7bbc5ff6
brazillierjo <j.brazillier@gmail.com>
2022-08-07 11:38:25 +0200

    [Homepage] add possibility to add new muscle in program

--- src/components/ProgramForm.tsx
+++ src/components/ProgramForm.tsx
@@ -1,10 +1,12 @@
 import { Formik, Form, Field } from "formik";
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { getAllMuscles, numberOfExercices } from "../helpers/dataModuler";
 import { setInLocalStorage } from "../helpers/localStorageHandler";
 
 export const ProgramForm: React.FC<{}> = () => {
+  const [muscleProgram, setMuscleProgram] = useState(1);
+
   const navigate = useNavigate();
 
   const listOfMuscles = getAllMuscles();
@@ -26,8 +28,20 @@ export const ProgramForm: React.FC<{}> = () => {
         initialValues={initialValues}
         onSubmit={(values) => handleSubmit(values)}
       >
-        <Form id="container">
-          {addMuscle(listOfMuscles)}
+        <Form>
+          <div id="container">
+            {[...Array(muscleProgram)].map((n, i) => (
+              <DisplayMuscleInProgram key={i} />
+            ))}
+          </div>
+
+          <button
+            onClick={() => setMuscleProgram(muscleProgram + 1)}
+            type="button"
+            className="rounded-md border"
+          >
+            Add
+          </button>
 
           <div className="text-center">
             <button
@@ -43,25 +57,29 @@ export const ProgramForm: React.FC<{}> = () => {
   );
 };
 
-const addMuscle = (listOfMuscles: string[]) => {
+export const DisplayMuscleInProgram: React.FC<{}> = () => {
+  const listOfMuscles = getAllMuscles();
+
   return (
-    <div className="my-4 grid grid-cols-5 justify-between gap-5">
-      <Field
-        as="select"
-        className="col-span-3 rounded-lg bg-lightGray p-3"
-        name="muscle"
-      >
-        {listOfMuscles.map((muscle) => (
-          <option key={muscle}>{muscle}</option>
-        ))}
-      </Field>
-      <Field
-        as="select"
-        className="col-span-2 rounded-lg bg-lightGray p-3"
-        name="numberOfExercices"
-      >
-        {numberOfExercices(10)}
-      </Field>
-    </div>
+    <>
+      <div className="my-4 grid grid-cols-5 justify-between gap-5">
+        <Field
+          as="select"
+          className="col-span-3 rounded-lg bg-lightGray p-3"
+          name="muscle"
+        >
+          {listOfMuscles.map((muscle) => (
+            <option key={muscle}>{muscle}</option>
+          ))}
+        </Field>
+        <Field
+          as="select"
+          className="col-span-2 rounded-lg bg-lightGray p-3"
+          name="numberOfExercices"
+        >
+          {numberOfExercices(10)}
+        </Field>
+      </div>
+    </>
   );
 };
