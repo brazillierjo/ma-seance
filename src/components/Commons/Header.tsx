5241c6e25c0aad42fd03ad5825508f6cde601b2d
brazillierjo <j.brazillier@gmail.com>
2022-08-13 22:50:35 +0200

    [Program] display program asked

--- src/components/Commons/Header.tsx
+++ src/components/Commons/Header.tsx
@@ -11,59 +11,61 @@ import { AiOutlineHome } from "react-icons/ai";
 export default function Header() {
 
     return (
-        <>
-            <div
-                className="flex h-12 w-screen items-center justify-between border-b-2 border-indigo-500 px-4 shadow-lg"
-            >
-                <Link to={"/"}><CgGym /></Link>
-                <div>
-                    <Menu as="div" className="relative inline-block text-left">
-                        <Menu.Button>
-                            <HiMenuAlt3 size={33} />
-                        </Menu.Button>
-                        <Transition
-                            as={Fragment}
-                            enter="transition ease-out duration-100"
-                            enterFrom="transform opacity-0 scale-95"
-                            enterTo="transform opacity-100 scale-100"
-                            leave="transition ease-in duration-75"
-                            leaveFrom="transform opacity-100 scale-100"
-                            leaveTo="transform opacity-0 scale-95"
-                        >
-                            <Menu.Items
-                                className="absolute bg-white z-30 right-0 mt-2 w-56 origin-top-right space-y-8 rounded-md px-5 py-2 shadow-lg">
+      <>
+        <div className="flex h-12 items-center justify-between border-b-2 border-indigo-500 px-4 shadow-lg">
+          <Link to={"/"}>
+            <CgGym />
+          </Link>
+          <div>
+            <Menu as="div" className="relative inline-block text-left">
+              <Menu.Button>
+                <HiMenuAlt3 size={33} />
+              </Menu.Button>
+              <Transition
+                as={Fragment}
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
+              >
+                <Menu.Items className="absolute right-0 z-30 mt-2 w-56 origin-top-right space-y-8 rounded-md bg-white px-5 py-2 shadow-lg">
+                  <Menu.Item>
+                    <Link to={"/"}>
+                      <button className="flex items-center py-2">
+                        <AiOutlineHome
+                          color="#4338ca"
+                          size={25}
+                          className="mr-3"
+                        />
+                        Home
+                      </button>
+                    </Link>
+                  </Menu.Item>
 
-                                <Menu.Item>
-                                    <Link to={"/"}>
-                                        <button className="flex items-center py-2">
-                                            <AiOutlineHome color="#4338ca" size={25} className="mr-3" />
-                                            Home
-                                        </button>
-                                    </Link>
-                                </Menu.Item>
+                  <Menu.Item>
+                    <Link to={"/exercices"}>
+                      <button className="flex items-center py-2">
+                        <FcTodoList size={25} className="mr-3" />
+                        Exercices
+                      </button>
+                    </Link>
+                  </Menu.Item>
 
-                                <Menu.Item>
-                                    <Link to={"/exercices"}>
-                                        <button className="flex items-center py-2">
-                                            <FcTodoList size={25} className="mr-3" />
-                                            Exercices
-                                        </button>
-                                    </Link>
-                                </Menu.Item>
-
-                                <Menu.Item>
-                                    <Link to={"/programme"}>
-                                        <button className="flex items-center py-2">
-                                            <CgGym color="#6366f1" size={25} className="mr-3" />
-                                            Programme
-                                        </button>
-                                    </Link>
-                                </Menu.Item>
-                            </Menu.Items>
-                        </Transition>
-                    </Menu>
-                </div>
-            </div>
-        </>
+                  <Menu.Item>
+                    <Link to={"/programme"}>
+                      <button className="flex items-center py-2">
+                        <CgGym color="#6366f1" size={25} className="mr-3" />
+                        Programme
+                      </button>
+                    </Link>
+                  </Menu.Item>
+                </Menu.Items>
+              </Transition>
+            </Menu>
+          </div>
+        </div>
+      </>
     );
 };
--- src/components/GeneratedProgram.tsx
+++ src/components/GeneratedProgram.tsx
@@ -1,13 +1,4 @@
-import {
-  JSXElementConstructor,
-  Key,
-  ReactElement,
-  ReactFragment,
-  ReactPortal,
-  useEffect,
-  useMemo,
-  useState
-} from "react";
+import { useEffect, useMemo, useState } from "react";
 import { getFromLocalStorage } from "../helpers/localStorageHandler";
 import exercices from "../data/workoutexercices.json";
 
@@ -41,25 +32,71 @@ export const GeneratedProgram: React.FC<{}> = () => {
 
   return (
     <>
-      {
-        // display name of each object in each array
-        program &&
-          program.map((day, index) => {
+      {program &&
+        program.map((group: any, index: any) => {
+          console.log(typeof group);
+
+          if (group) {
             return (
-              <div key={index}>
-                <h2>Day {index + 1}</h2>
-                {day.map((exercice, index) => {
-                  return (
-                    <div key={index}>
-                      <h3>{exercice.name}</h3>
-                      <p>{exercice.description}</p>
-                    </div>
-                  );
-                })}
+              <div
+                className="mx-2 my-4 rounded-md border-4 border-primary bg-lightGray p-1"
+                key={index}
+              >
+                <h2 className="px-4 text-2xl font-bold italic">
+                  {group[0] && group[0].id}
+                </h2>
+                {group &&
+                  group.map((exercice: any, index: any) => {
+                    return (
+                      <div
+                        className="my-4 mx-4 flex rounded bg-white p-4"
+                        key={index}
+                      >
+                        <img
+                          className="w-4/12 rounded"
+                          src={`./assets/${exercice.path}`}
+                          alt={exercice.name}
+                        />
+                        <div className="flex w-8/12 flex-col rounded bg-lightGray p-3">
+                          <h3 className="mb-2 text-lg font-bold">
+                            _{exercice.name}
+                          </h3>
+                          <p className="text-xm">
+                            Séries :{" "}
+                            <span className="font-bold">
+                              {Math.floor(Math.random() * (5 - 3 + 1) + 3)}
+                            </span>
+                          </p>
+                          <p className="text-xm">
+                            Réps :{" "}
+                            <span className="font-bold">
+                              {Math.floor(Math.random() * (15 - 8 + 1) + 8)}
+                            </span>
+                          </p>
+                          <div className="mt-auto flex flex-wrap">
+                            {exercice.secondary &&
+                              exercice.secondary.map(
+                                (secondary: any, index: any) => {
+                                  return (
+                                    <p
+                                      className="m-1 w-fit rounded bg-white px-2 py-1"
+                                      key={index}
+                                    >
+                                      {secondary}
+                                    </p>
+                                  );
+                                }
+                              )}
+                          </div>
+                        </div>
+                      </div>
+                    );
+                  })}
               </div>
             );
-          })
-      }
+          }
+          return null;
+        })}
     </>
   );
 };
